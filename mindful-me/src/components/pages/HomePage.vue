7d33345d833cd108bef4e7981f3743a1a1655a65
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-03-15 20:15:13 +1030

    basic home page structure

--- mindful-me/src/components/pages/HomePage.vue
+++ mindful-me/src/components/pages/HomePage.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>
-        qwerty
-    </div>
+  <header class="sticky top-0">
+    <div class="uppercase font-bold text-6xl">mindfulme</div>
+  </header>
+  <div>
+    <div class="font-semibold text-3xl">Welcome to MindfulMe!</div>
+    <div class="text-base">We are thrilled that you have chosen us.</div>
+    <div class="uppercase">signup</div>
+    <div class="uppercase">signin</div>
+    <div>Created by John, Dhanush, Alex and Vicky</div>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
