5195e5943507034efa74a15e34aa7ba78048bb71
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-03-15 21:47:07 +1030

    added google fonts

--- mindful-me/tailwind.config.js
+++ mindful-me/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        doto: ['Doto', 'sans-serif'],
+        spaceMono: ['Space Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
-
