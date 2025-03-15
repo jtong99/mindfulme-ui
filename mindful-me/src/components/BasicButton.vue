2ccdeed91cbc36b5005a92847e62feb28413af2d
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-03-15 21:53:54 +1030

    basic css

---
+++ mindful-me/src/components/BasicButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    @click="handleClick"
+    :disabled="disabled"
+    class="border font-semibold py-2 px-4 rounded-lg shadow-md"
+    :class="btnClass"
+  >
+    <div>{{ props.label }}</div>
+  </button>
+</template>
+
+<script setup>
+const emits = defineEmits(['emitClick'])
+const props = defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+  action: {
+    type: String,
+    required: true,
+  },
+  btnClass: {
+    type: String,
+  },
+  width: {
+    type: String,
+    default: "w-28"
+  }
+})
+
+const handleClick = () => {
+  emits('emitClick', props.action)
+  console.log('emit', props.action)
+}
+</script>
--- mindful-me/src/components/pages/HomePage.vue
+++ mindful-me/src/components/pages/HomePage.vue
@@ -1,14 +1,24 @@
 <template>
-  <header class="sticky top-0">
-    <div class="uppercase font-bold text-6xl">mindfulme</div>
+  <header class="sticky top-0 py-7">
+    <div class="uppercase font-bold text-6xl justify-self-center font-doto">mindful me</div>
   </header>
-  <div>
+  <div class="flex flex-col gap-6 items-center font-mono py-7">
     <div class="font-semibold text-3xl">Welcome to MindfulMe!</div>
-    <div class="text-base">We are thrilled that you have chosen us.</div>
-    <div class="uppercase">signup</div>
-    <div class="uppercase">signin</div>
-    <div>Created by John, Dhanush, Alex and Vicky</div>
+    <div class="text-base mono">We are thrilled that you have chosen us.</div>
+    <BasicButton
+      label="Sign Up"
+      action="sign-up"
+      btnClass="bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white border-blue-500 hover:border-transparent"
+    />
+    <BasicButton
+      label="Sign In"
+      action="sign-in"
+      btnClass="bg-red-500 hover:bg-transparent text-white hover:text-red-700 border-transperant hover:border-red-500"
+    />
+    <div class="text-xs">Created by John, Dhanush, Alex and Vicky</div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import BasicButton from '../BasicButton.vue'
+</script>
